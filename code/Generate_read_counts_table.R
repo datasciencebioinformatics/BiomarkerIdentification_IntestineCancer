4ae17c7f4f1eeab0c3765b4506e67d0e411c20ea
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2026-04-01 09:12:32 -0500

    Create Generate_read_counts_table.R

---
+++ code/Generate_read_counts_table.R
@@ -0,0 +1,20 @@
+###################################################################################
+# Set the project configuration
+# Se the path to output folder
+project_folder="/home/felipe/Documentos/BiomarkerIdentification_ProstateCancer/"
+
+# Set the path to manifest file
+Manifest_file=paste(project_folder,"gdc_manifest.2026-04-01.072557.txt",sep="")
+
+# Set the path to metadata cohort file
+Metadata_file=paste(project_folder,"metadata.cohort.2026-04-01.json",sep="")
+
+# Set the path to Sample sheet file
+Sample_sheet_file=paste(project_folder,"gdc_sample_sheet.2026-04-01.tsv",sep="")
+
+# Set the path to biospecimen cohort file
+Biospecimen_file=paste(project_folder,"biospecimen.cohort.2026-04-01.tar.gz",sep="")
+
+# Set the path to clinical cohort file
+Clinical_file=paste(project_folder,"clinical.cohort.2026-04-01.tar.gz",sep="")
+###################################################################################
