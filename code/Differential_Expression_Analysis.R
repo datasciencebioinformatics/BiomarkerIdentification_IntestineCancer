70236aed31ce593f300eaf6b59989fd3e7a57abd
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2026-04-02 10:50:10 -0500

    Update Differential_Expression_Analysis.R

--- code/Differential_Expression_Analysis.R
+++ code/Differential_Expression_Analysis.R
@@ -1,4 +1,9 @@
 # Create DESeqDataSet from your prepared matrix
 dds <- DESeqDataSetFromMatrix(countData = read_counts_table,
                               colData = sample_sheet_data,
-                              design = ~ Tissue.Type)
+
+# Run DeSeq2
+dds <- DESeq(dds)
+
+# Obtain the results
+res <- results(dds, contrast=c("Tissue.Type","Tumor","Normal"))
