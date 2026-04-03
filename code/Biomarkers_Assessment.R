958184088739709b02b04c5639c645c8628f1fbf
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2026-04-03 15:19:23 -0300

    Update Biomarkers_Assessment.R

--- code/Biomarkers_Assessment.R
+++ code/Biomarkers_Assessment.R
@@ -10,14 +10,13 @@ percentVar <- round(100 * attr(pcaData, "percentVar"))
 
 # Melt tabele
 # Plot_raw_vibration_data.png                                                                                                            
-png(filename=paste(project_folder,"PCA_Plot_of_RNASeq_Samples_TissueType.png",sep=""), width = 15, height = 20, res=600, units = "cm")  
+png(filename=paste(project_folder,"PCA_Plot_of_RNASeq_Samples_TissueType.png",sep=""), width = 15, height = 15, res=600, units = "cm")  
   # 3. Create custom ggplot
-  ggplot(pcaData, aes(x = PC1, y = PC2, color = "Tissue.Type")) +
+  ggplot(pcaData, aes(x = PC1, y = PC2, color = group)) +
     geom_point(size = 3) +
     xlab(paste0("PC1: ", percentVar[1], "% variance")) +
     ylab(paste0("PC2: ", percentVar[2], "% variance")) +
     coord_fixed() +
-    ggtitle("PCA Plot of RNA-seq Samples") +
     theme_minimal()
 dev.off()
 
