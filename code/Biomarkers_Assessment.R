5edd8a8ac80068817acc8ab255000488bcfa8f75
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2026-04-07 11:40:22 -0300

    Update Biomarkers_Assessment.R

--- code/Biomarkers_Assessment.R
+++ code/Biomarkers_Assessment.R
@@ -20,4 +20,44 @@ png(filename=paste(project_folder,"PCA_Plot_of_RNASeq_Samples_TissueType.png",se
     theme_minimal()
 dev.off()
 
+#######################################################################################################################################
+# Take the normal samples, tumor samples, primary samples, metastic samples
+normal_sample_ids     <- rownames(sample_sheet_data[which(sample_sheet_data$Tissue.Type     == "Normal"),])
+tumor_sample_ids      <- rownames(sample_sheet_data[which(sample_sheet_data$Tissue.Type     == "Tumor"),])
+primary_sample_ids    <- rownames(sample_sheet_data[which(sample_sheet_data$Tumor.Descriptor=="Primary"),])
+metastatic_sample_ids <- rownames(sample_sheet_data[which(sample_sheet_data$Tumor.Descriptor=="Metastatic"),])
+
+# Add gene id and gene symbol to table
+cbind(gene_id=rownames(read_counts_table_tpm,read_counts_table_tpm)
+
+# Biomarkers whose fold change (FC) was ≥50 and average TPM of control samples ≤ 10.
+# First, compile data.frame with 
+# Take p-value
+df_mean<-data.frame(
+    avg.normal=rowMeans(read_counts_table_tpm[res_tumor_normal$gene,normal_sample_ids]),
+    std.normal=0,
+    avg.tumor=rowMeans(read_counts_table_tpm[res_tumor_normal$gene,tumor_sample_ids]),
+    std.tumor=0, 
+    avg.primary=rowMeans(read_counts_table_tpm[res_tumor_normal$gene,primary_sample_ids]), 
+    std.primary=0, 
+    avg.metastatic=rowMeans(read_counts_table_tpm[res_tumor_normal$gene,metastatic_sample_ids]), 
+    std.metastatic=0)
+
+# For each gene, calculate the std too the 
+for (gene in rownames(df_mean))
+{
+  df_mean[gene,"std.normal"]<-sd(unstranded_data[gene,normal_sample_ids])
+  df_mean[gene,"std.tumor"]<-sd(unstranded_data[gene,tumor_sample_ids])
+  df_mean[gene,"std.primary"]<-sd(unstranded_data[gene,primary_sample_ids])
+  df_mean[gene,"std.metastatic"]<-sd(unstranded_data[gene,metastatic_sample_ids])  
+}      
+
+# Add the Gene smbol to the table      
+
+
+# Save list
+sheets_list <- list("tumor_genes"= res_tumor_normal, "primary_tumor_genes" = res_Primary_normal, "metastatic_tumor_genes"=res_Metastatic_normal)
+write_xlsx(sheets_list,paste(project_folder,"Supplemental_Table_S1.xlsx",sep="" ))
+
+
 
