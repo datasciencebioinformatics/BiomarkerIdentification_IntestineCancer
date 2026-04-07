344c4d840dbc9e0b2a640b70d760e6f784e8b6c0
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2026-04-07 11:43:31 -0300

    Update Generate_read_counts_table.R

--- code/Generate_read_counts_table.R
+++ code/Generate_read_counts_table.R
@@ -1,5 +1,10 @@
 ###################################################################################
 # Set the project configuration
+# Set the path to manifest file
+Manifest_file=paste(project_folder,"metadata/gdc_manifest.2026-04-05.155802.txt",sep="")
+
+# Set the path to Sample sheet file
+Sample_sheet_file=paste(project_folder,"metadata/gdc_sample_sheet.2026-04-05.tsv",sep="")
 
 # Set the path to Sample sheet filtered file
 Sample_sheet_filtered_file=paste(project_folder,"metadata/gdc_sample_sheet_star_gene_counts.tsv",sep="")
@@ -7,19 +12,23 @@ Sample_sheet_filtered_file=paste(project_folder,"metadata/gdc_sample_sheet_star_
 # Take the list of gene ids
 gene_ids_file<-paste(project_folder,"metadata/gene_ids.txt",sep="")
 ###################################################################################
-# Load data
-
 # Load sample sheet data
 sample_sheet_data<-read.delim(file = Sample_sheet_filtered_file, sep = '\t', header = TRUE,fill=TRUE)
 
 # Load gene ids data
 gene_ids_data<-read.delim(file = gene_ids_file, sep = '\t', header = TRUE,fill=TRUE)
 ###################################################################################
-# Star dataframe with all gene ids
+# Star dataframe with all gene ids unstranded
 read_counts_table<-data.frame(gene_id=as.vector(gene_ids_data[,1]))
 
-# Set rownames
+# Star dataframe with all gene ids tpm_unstranded
+read_counts_table_tpm<-data.frame(gene_id=as.vector(gene_ids_data[,1]))
+
+# Set rownames unstranded
 rownames(read_counts_table)<-read_counts_table$gene_id
+
+# Set rownames tpm_unstranded
+rownames(read_counts_table_tpm)<-read_counts_table_tpm$gene_id
 
 # Set rownames of 
 rownames(sample_sheet_data)<-sample_sheet_data$File.ID
@@ -42,16 +51,24 @@ for (sample in rownames(sample_sheet_data))
   # Set rownames
   rownames(counts_table)<-counts_table$gene_id
 
-  # Start data.frame for the table
-  df_counts_table<-data.frame(file_id=counts_table[rownames(counts_table),"unstranded"])
+  # Start data.frame for the table unstranded
+  df_counts_table_unstranded<-data.frame(file_id=counts_table[rownames(counts_table),"unstranded"])
+
+  # Start data.frame for the table unstranded
+  df_counts_table_tpm<-data.frame(file_id=counts_table[rownames(counts_table),"tpm_unstranded"])  
 
   # Set file_id in the colname
-  colnames(df_counts_table)<-file_id
+  colnames(df_counts_table_unstranded)<-file_id
+  colnames(df_counts_table_tpm)<-file_id
 
-  # Add to table
-  read_counts_table<-cbind(read_counts_table,df_counts_table)
+  # Add to table unstranded
+  read_counts_table<-cbind(read_counts_table,df_counts_table_unstranded)
+
+  # Add to table tpm_unstranded
+  read_counts_table_tpm<-cbind(read_counts_table_tpm,df_counts_table_tpm)  
 }
-# Remove first collumn
+# Remove first collumn unstranded
 read_counts_table<-read_counts_table[,-1]
 
-
+# Remove first collumn tpm_unstranded
+read_counts_table_tpm<-read_counts_table_tpm[,-1]
