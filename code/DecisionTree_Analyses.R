c2da1dc8ff75ccac3fac382c13e13ee2340053b4
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2026-04-09 14:11:35 -0300

    Update DecisionTree_Analyses.R

--- code/DecisionTree_Analyses.R
+++ code/DecisionTree_Analyses.R
@@ -18,7 +18,7 @@ trainning_set_ids<-sample(colnames(read_counts_table_tpm), size=round(length(col
 testing_set_ids  <-colnames(read_counts_table_tpm)[!colnames(read_counts_table_tpm) %in% trainning_set_ids]
 
 # Compute rpart model 
-Tissue_Type_rpart<-rpart(formula=Tissue_Type ~ ., data=data.frame(read_counts_table_tpm_disc[trainning_set_ids,]),method = "class")
+Tissue_Type_rpart<-rpart(formula=Tissue_Type ~ ., data=data.frame(read_counts_table_tpm_disc),method = "class")
 
 # bwplot               
 png(filename=paste(output_dir,"rpart_Tissue_Type.png",sep=""), width = 15, height = 15, res=600, units = "cm")  
@@ -27,11 +27,5 @@ png(filename=paste(output_dir,"rpart_Tissue_Type.png",sep=""), width = 15, heigh
 dev.off()
 
 #########################################################################################################
-# Once the surrogate model is fitted with the trainning set, the RMSE, MAE and mean relative error are calculated from the testing.
-predicted_testing_set <- as.vector(predict(Tissue_Type_rpart, newdata = data.frame(read_counts_table_tpm_disc[testing_set_ids,])))
 
-rmse=rmse(actual_testing_set, predicted_testing_set), 
-mae=mae(actual_testing_set, predicted_testing_set),
-mre=mre(actual_testing_set, predicted_testing_set), 
-cor=cor(actual_testing_set, predicted_testing_set),
 
